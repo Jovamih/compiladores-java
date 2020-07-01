92f6c8033b5eea2deb6011d8da04f14e4fb69745
Jovamih <avira365@hotmail.com>
2020-07-01 00:28:38 -0500

    Cabios  importantes  a la clase Tokenizer

--- compiladores-java-source/src/modelo/Token.java
+++ compiladores-java-source/src/modelo/Token.java
@@ -22,5 +22,13 @@ public class Token {
     public String getValue() {
         return value;
     }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
     
 }
--- compiladores-java-source/src/modelo/Tokenizer.java
+++ compiladores-java-source/src/modelo/Tokenizer.java
@@ -5,10 +5,99 @@
  */
 package modelo;
 
-/**
- *
- * @author JOHAN
- */
 public class Tokenizer {
+    private int currentIndex;
+    private int currentLine;
+    private String cadena;
+    private String ncad;
+    private boolean hasVariable;
+    private boolean hasMacro;
+    private String lastType;
+    private String lastMacro;
+    public Tokenizer(String cadena){
+        this.cadena=cadena;
+    }
+    public Token nextToken(){
+        Token token = new Token();
+        char character=this.cadena.charAt(this.currentIndex);
+        if(Character.isLetter(character)){
+            while(Character.isLetter(character) && Character.isDigit(character)){
+                token.setValue(token.getValue()+character);
+                this.currentIndex++;
+                if(this.cadena.length()>this.currentIndex)
+                        character=this.cadena.charAt(this.currentIndex);
+                else break;
+            }
+            String subtype=this.searchToken(token.getValue(),"PR");
+            token.setType(subtype!=null ?subtype:"ID");
+      
+        }
+        else if("\"'".indexOf(character)!=-1){
+            token.setValue(token.getValue()+character);
+            this.currentIndex++;
+            character=this.cadena.charAt(this.currentIndex);
+            while("\"'".indexOf(character)==-1){
+                token.setValue(token.getValue()+character);
+                this.currentIndex++;
+                character=this.cadena.charAt(this.currentIndex);
+            }
+            token.setValue(token.getValue()+character);
+            this.currentIndex++;
+            token.setType("CAD");
+            
+        }else if(Character.isDigit(character)){
+            while(Character.isDigit(character)){
+                token.setValue(token.getValue()+character);
+                this.currentIndex++;
+                if (this.cadena.length()>this.currentIndex)
+                    character=this.cadena.charAt(this.currentIndex);
+                else break;
+            }
+            token.setType("NUM");
+        }else{
+            String subtype=null;
+            if((subtype=this.searchToken(String.valueOf(character),"OP"))!=null){
+                token.setType(subtype);
+                token.setValue(token.getValue()+character);
+                this.currentIndex++;
+                char nextChar= this.cadena.length()>this.currentIndex ? this.cadena.charAt(this.currentIndex):0;
+                String doubleOperator= String.valueOf(character)+String.valueOf(nextChar);
+               if((subtype=this.searchToken(doubleOperator,"OP"))!=null){
+                    token.setValue(token.getValue()+character);
+                    this.currentIndex++;
+                    token.setType(subtype);
+             }
+            }
+            //special en c++
+            else if(character=='#'){
+                token.setValue(token.getValue()+character);
+                this.currentIndex++;
+                while(this.cadena.charAt(this.currentIndex)==' ') this.currentIndex++;
+                if(Character.isLetter(this.cadena.charAt(currentIndex)) || this.cadena.charAt(currentIndex)=='_'){
+                    while(Character.isLetter(this.cadena.charAt(currentIndex)) || this.cadena.charAt(currentIndex)=='_'){
+                        character=this.cadena.charAt(currentIndex);
+                        token.setValue(token.getValue()+character);
+                        this.currentIndex++;
+                        if(this.cadena.length()>currentIndex) character=this.cadena.charAt(currentIndex);
+                        else break;
+                    }
+                   // if(token.getValue().toLowerCase())
+                   //lo suspedemos para mas rato
+                }else System.out.println("Error lexico No se reconoce");
+            }else System.out.println("Error lexico no s reconoce");
+            
+            
+            
+            
+            
+            
+            
+        }
+        
+        return token;
     
+    }
+    private String searchToken(String value, String type){
+            return null;
+    }
 }
